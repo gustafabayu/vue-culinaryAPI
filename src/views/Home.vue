71b0015816c7cd08247fd219f37888277766945a
Makibo93 <makibo93@users.noreply.github.com>
2021-04-02 20:40:59 +0700

    card product

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,6 +3,26 @@
     <Navbar />
     <div class="container">
       <Hero />
+      <div class="row mt-4">
+        <div class="col">
+          <h2>Best <strong>Menus</strong></h2>
+        </div>
+        <div class="col">
+          <router-link to="/foods" class="btn btn-success float-right"
+            ><b-icon-eye></b-icon-eye> Shows..</router-link
+          >
+        </div>
+      </div>
+
+      <div class="row mb-3">
+        <div
+          class="col-md-3 mt-4"
+          v-for="product in products"
+          :key="product.id"
+        >
+          <CardProduct :product="product" />
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -11,12 +31,38 @@
 // @ is an alias to /src
 import Navbar from "@/components/Navbar.vue";
 import Hero from "@/components/Hero.vue";
+import CardProduct from "@/components/CardProduct.vue";
+import axios from "axios";
 
 export default {
   name: "Home",
   components: {
     Navbar,
     Hero,
+    CardProduct,
+  },
+  data() {
+    return {
+      products: [],
+    };
+  },
+  methods: {
+    setProduct(data) {
+      this.products = data;
+    },
+  },
+  mounted() {
+    axios
+      .get("http://localhost:3000/best-products")
+      .then(function (response) {
+        // handle success
+        this.setProducts(response.data);
+        // console.log("Success : ", response);
+      })
+      .catch(function (error) {
+        // handle error
+        console.log("Error : ", error);
+      });
   },
 };
 </script>
