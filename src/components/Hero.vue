61c8023fe782c168cf933773ce30dce5aaa69601
Makibo93 <makibo93@users.noreply.github.com>
2021-04-02 14:36:38 +0700

    home view

---
+++ src/components/Hero.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="row mt-4">
+    <div class="col">
+      <div class="d-flex h-100">
+        <div class="justify-content-center align-self-center">
+          <h2><strong>Delicious Foods</strong><br />In Your Hands</h2>
+          <p>Pick Your Foods Now!!</p>
+          <!-- <router-link class="btn btn-lg btn-success">Order</router-link> -->
+          <button class="btn btn-lg btn-success">
+            <b-icon-arrow-right></b-icon-arrow-right> Order
+          </button>
+        </div>
+      </div>
+    </div>
+    <div class="col">
+      <img src="../assets/image/ilustrasi.png" width="100%" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Hero",
+};
+</script>
+
+<style>
+</style>
